739e6fb423d2eaf9d99dbc80609e129efad7558e
Szekely Attila <attila@wolfpack-digital.com>
2020-11-07 01:54:47 +0200

    Add mailer config 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = ENV['DOMAIN']
 
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
