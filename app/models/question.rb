c22d4e07a9b7bd9786281dad11635022d1930b5d
Szekely Attila <attila@wolfpack-digital.com>
2020-11-07 14:55:07 +0200

    Improvements 2

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,11 @@
 class Question < ApplicationRecord
   belongs_to :animal
+
+  validates :body, presence: true
+  validates :correct_answer, presence: true
+  validate :ensure_min_1_wrong_answer
+
+  def ensure_min_1_wrong_answer
+    errors.add(:answer1, 'can\'t be blank') if [answer1, answer2, answer3].reject(&:empty?).blank?
+  end
 end
