a8164ba6a62cffe89e3dff2d89eb69bd1ac2f97e
Szekely Attila <attila@wolfpack-digital.com>
2020-11-07 23:21:20 +0200

    Photo and model urls

--- app/serializers/api/animal_serializer.rb
+++ app/serializers/api/animal_serializer.rb
@@ -1,3 +1,9 @@
 class Api::AnimalSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :donate_url
+  attributes :id, :name, :description, :donate_url, :model_url
+
+  def model_url
+    return if object.model.blank?
+
+    Rails.application.routes.url_helpers.rails_blob_url(object.model, host: ENV['DOMAIN'] || 'localhost:3000')
+  end
 end
--- app/serializers/api/track_serializer.rb
+++ app/serializers/api/track_serializer.rb
@@ -1,3 +1,9 @@
 class Api::TrackSerializer < ActiveModel::Serializer
-  attributes :id, :name, :city, :description, :check_points_count
+  attributes :id, :name, :city, :description, :check_points_count, :photo_url
+
+  def photo_url
+    return if object.photo.blank?
+
+    Rails.application.routes.url_helpers.rails_blob_url(object.photo, host: ENV['DOMAIN'] || 'localhost:3000')
+  end
 end
